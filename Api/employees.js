a71a3748b86679a2f93bbdb065d35b6b75595cfc
priyank099 <vorapriyank059@gmail.com>
2019-08-01 15:27:37 +0530

    Update employees.js

--- Api/employees.js
+++ Api/employees.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const crypto= require('crypto');
 
+//add new employee
 router.post('/registeremp',function(req,res){
   var employee={
     "emp_name":req.body.emp_name,
@@ -15,6 +16,14 @@ router.post('/registeremp',function(req,res){
     const key= crypto.pbkdf2Sync(emp.emp_pass,'salt',100,10,'sha512');
     emp.emp_pass= key.toString('hex');
       
+    connection.query(`select count(emp_email) as count from employees where emp_email='${req.body.emp_email}'`,function (error,results){
+      if(error){
+        console.log(error);
+      }
+      console.log(results);
+      if(results[0].count > 0){
+        res.status(400).json('Employee already exists');
+      }else{
     connection.query('insert into employees set ?',emp,function (error, results, fields) {
         if (error) {
           res.json({
@@ -30,8 +39,9 @@ router.post('/registeremp',function(req,res){
           })
         }
       });
+    }
+    });
 });
-
 
 
 module.exports=router;
